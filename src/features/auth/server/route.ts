d3c95486ed5395378da1fae30b7ad8b6e015a492
Vihanga Rashan Silva <vihangarashansilva@gmail.com>
2024-11-03 09:34:54 +0530

    Add: Appwrite Client + Auth API Functionalities

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,23 +1,77 @@
 import { Hono } from "hono";
+import { ID } from "node-appwrite";
 import { zValidator } from "@hono/zod-validator";
+import { deleteCookie, setCookie } from "hono/cookie";
+
+import { createAdminClient } from "@/lib/appwrite";
 
 import { signInSchema } from "@/features/auth/schemas/sign-in.schema";
 import { signUpSchema } from "@/features/auth/schemas/sign-up.schema";
+import { AUTH_COOKIE } from "../constants";
 
 const app = new Hono()
+  /**
+   * Login API Handler
+   */
   .post("/login", zValidator("json", signInSchema), async (c) => {
     const { email, password } = c.req.valid("json");
 
-    console.log({ email, password });
+    // Appwrite login methology
+    const { account } = await createAdminClient();
+
+    const session = await account.createEmailPasswordSession(email, password);
+
+    // Session Cookie handelling (with hono/cookie)
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      // secure: process.env.NODE_ENV === "production",
+      secure: true,
+      sameSite: "strict",
+      maxAge: 60 * 60 * 24 * 30,
+    });
 
     return c.json({ email, password });
   })
+
+  /**
+   * Register API Handler
+   */
   .post("/register", zValidator("json", signUpSchema), async (c) => {
     const { email, name, password } = c.req.valid("json");
 
-    console.log({ email, name, password });
+    // Appwrite signup methology
+    const { account } = await createAdminClient();
 
-    return c.json({ email, name, password });
+    const user = await account.create(ID.unique(), email, password, name);
+
+    /**
+     * This is not essential for signup process.
+     * But, if you want to login the user after signup, you can use this code.
+     */
+    const session = await account.createEmailPasswordSession(email, password);
+
+    // Session Cookie handelling (with hono/cookie)
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      // secure: process.env.NODE_ENV === "production",
+      secure: true,
+      sameSite: "strict",
+      maxAge: 60 * 60 * 24 * 30,
+    });
+
+    return c.json({ success: true, data: user });
+  })
+
+  /**
+   * Register API Handler
+   */
+  .post("/logout", (c) => {
+    // Session Cookie handelling (with hono/cookie)
+    deleteCookie(c, AUTH_COOKIE);
+
+    return c.json({ success: true });
   });
 
 export default app;
--- timestamp.md
+++ timestamp.md
@@ -1,2 +1,12 @@
 Topic: Adding Hono Auth API + End-to-End TypeSafty with RPC
 https://youtu.be/Av9C7xlV0fA?t=6780 to https://youtu.be/Av9C7xlV0fA?t=8325
+
+---
+
+Topic: Appwrite Signup Process
+https://youtu.be/Av9C7xlV0fA?t=9492
+
+---
+
+Topic: Building the session middleware
+https://youtu.be/Av9C7xlV0fA?t=10270
